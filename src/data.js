d82d032fe32ada15388d9c5ef8394c8b4a5db061
Chris Fonseca <fonseca.j.chris@gmail.com>
2022-01-10 16:49:07 -0500

    Updated Projects page with Roman Website

--- src/data.js
+++ src/data.js
@@ -1,27 +1,33 @@
 export const projects = [
-  {
-    title: "SwingBeat Landing Page",
-    subtitle: "Bootstrap, CSS, JS",
-    description: "Practice creating landing page.",
-    image: "./sbwalkthrough.gif",
-    link: "https://github.com/cfonseca2013/SimpleTodo",
-  },
-  {
-    title: "Rock, Paper, Scissors",
-    subtitle: "Javascript, HTML, CSS",
-    description: "This is a simple rock, papers, scissors game.",
-    image: "./rpcwalkthrough.gif",
-    link: "https://github.com/cfonseca2013/new-rock-paper-scissors-UI",
-  },
-  {
-    title: "Flixster",
-    subtitle: "Java, Android",
-    description:
-      "Simple movie app for Andoird. Created for Codepath project.",
-    image: "./flixpt2.gif",
-    link: "https://github.com/cfonseca2013/flixster",
-  },
-  /*
+	{
+		title: "Roman Fuller Personal/Business Website",
+		subtitle: "JavaScript, HTML, CSS",
+		description: "Created a personal/business website for client.",
+		image: "./roman.gif",
+		link: "https://romanfuller.netlify.app/",
+	},
+	{
+		title: "SwingBeat Landing Page",
+		subtitle: "Bootstrap, CSS, JavaScri[t",
+		description: "Practice creating landing page.",
+		image: "./sbwalkthrough.gif",
+		link: "https://github.com/cfonseca2013/SimpleTodo",
+	},
+	{
+		title: "Rock, Paper, Scissors",
+		subtitle: "JavaScript, HTML, CSS",
+		description: "This is a simple rock, papers, scissors game.",
+		image: "./rpcwalkthrough.gif",
+		link: "https://github.com/cfonseca2013/new-rock-paper-scissors-UI",
+	},
+	{
+		title: "Flixster",
+		subtitle: "Java, Android",
+		description: "Simple movie app for Andoird. Created for Codepath project.",
+		image: "./flixpt2.gif",
+		link: "https://github.com/cfonseca2013/flixster",
+	},
+	/*
   {
     title: "Epic Todo App",
     subtitle: "React Hooks",
@@ -33,10 +39,10 @@ export const projects = [
 ];
 
 export const skills = [
-  "JavaScript",
-  "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
+	"JavaScript",
+	"React",
+	"Next.js",
+	"Node",
+	"GraphQL",
+	"Material UI",
 ];
